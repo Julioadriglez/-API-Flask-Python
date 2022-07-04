077e64861cc421a9317be3bfd59a14d80257e8f5
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-07-04 17:43:30 -0500

    Se agrego  methods y se cambio logica de if else

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from flask import request #importa modulo request
 
 app = Flask(__name__) # Se crea servidor y se guarda en app
 
-@app.route('/<id>')   #  se define la dirección de la ruta 
+@app.route('/<id>', methods = ["GET", "POST"])   #  se define la dirección de la ruta 
 
 def saludo(id):  # imprime una acción como respuesta de la petición 
-    print(request)  # Imprime lo que pasa request y lo imprime en la consola 
-    if( id == "8"):
-        return " por que me pides 8"
+    print(request.method)  # Imprime lo que pasa request y lo imprime en la consola 
+    if( request.method == "POST"):
+        print("Guardado en la base de datos")
+        return " Guardado"
     else:
+        print("recurso obtenido")
         return  "El id que me pides es el "+id
 
 app.run(debug = True)
