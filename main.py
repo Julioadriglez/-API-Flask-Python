ad650f9398e985b769d7bdeb6271c54b0181de19
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-07-04 17:29:44 -0500

    Se agrega modulo request y se imprime en consola

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask  
+from flask import Flask    # Importa modulo Flask
+from flask import request #importa modulo request
 
 app = Flask(__name__) # Se crea servidor y se guarda en app
 
-@app.route('/app/v1/users')   #  se define la dirección de la ruta 
+@app.route('/<id>')   #  se define la dirección de la ruta 
 
-def user_action():  # imprime una acción como respuesta de la petición 
-    print("estoy aca")
-    return "tu usuario"
+def saludo(id):  # imprime una acción como respuesta de la petición 
+    print(request)  # Imprime lo que pasa request y lo imprime en la consola 
+    if( id == "8"):
+        return " por que me pides 8"
+    else:
+        return  "El id que me pides es el "+id
 
 app.run(debug = True)
