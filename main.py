45866f6cdb16761f0e356f2b09e88ec75d9c271f
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-07-04 16:17:00 -0500

    se definio primera rutas

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from flask import Flask  """ se importa flask de los modulos de python """
 
-app = Flask(__name__)
+app = Flask(__name__) # Se crea servidor y se guarda en app
+
+@app.rout('/app/v1/users')   #  se define la dirección de la ruta 
+
+def user_action():  # imprime una acción como respuesta de la petición 
+    print("estoy aca")
+    return "tu usuario"
 
 app.run(debug = True)
